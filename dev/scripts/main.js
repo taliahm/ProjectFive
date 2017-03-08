9faddbe4dedf70b65581ae1426c74e3db00ba529
Alex Ghattas <ghattas.alex@gmail.com>
2017-03-08 14:09:45 -0500

    edits to index and main.js

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -41,46 +41,66 @@ giftApp.occasions = [
 		
 	},{},{}];
 
-//Function to get user's location
-giftApp.getUserLocation = () => {
-	// function initMap() {
-	        var map = new google.maps.Map(document.getElementById('map'), {
-	          center: {lat: -34.397, lng: 150.644},
-	          zoom: 6
-	        });
-	        // var infoWindow = new google.maps.InfoWindow({map: map});
-	        // // Try HTML5 geolocation.
-	        //      if (navigator.geolocation) {
-	        //        navigator.geolocation.getCurrentPosition(function(position) {
-	        //          var pos = {
-	        //            lat: position.coords.latitude,
-	        //            lng: position.coords.longitude
-	        //          };
-
-	        //          infoWindow.setPosition(pos);
-	        //          infoWindow.setContent('Location found.');
-	        //          map.setCenter(pos);
-	        //        }, function() {
-	        //          handleLocationError(true, infoWindow, map.getCenter());
-	        //        });
-	        //      } else {
-	        //        // Browser doesn't support Geolocation
-	        //        handleLocationError(false, infoWindow, map.getCenter());
-	        //      }
-	        //    }
-
-	        //    function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-	        //      infoWindow.setPosition(pos);
-	        //      infoWindow.setContent(browserHasGeolocation ?
-	        //                            'Error: The Geolocation service failed.' :
-	        //                            'Error: Your browser doesn\'t support geolocation.');
-	           // }
-	
+giftApp.getLcboProductReturn = () => {
+	$.ajax({
+	    url: 'http://proxy.hackeryou.com',
+	    dataType: 'json',
+	    method:'GET',
+	    data: {
+	        reqUrl: 'http://lcboapi.com/products',
+	        params: {
+	            key: giftApp.lcboKey,
+	            per_page: 100,
+	        },
+	        xmlToJSON: false
+	    }
+	})
+	// Stores the data from the array into a variable
+	.then(function(result) {
+		let productResult = result;
+		console.log('Product results', productResult);
+	});
 }
 
+//Function to get user's location
+// giftApp.getUserLocation = () => {
+// 	// function initMap() {
+// 	        var map = new google.maps.Map(document.getElementById('map'), {
+// 	          center: {lat: -34.397, lng: 150.644},
+// 	          zoom: 6
+// 	        });
+// 	        var infoWindow = new google.maps.InfoWindow({map: map});
+// 	        // Try HTML5 geolocation.
+// 	             if (navigator.geolocation) {
+// 	               navigator.geolocation.getCurrentPosition(function(position) {
+// 	                 var pos = {
+// 	                   lat: position.coords.latitude,
+// 	                   lng: position.coords.longitude
+// 	                 };
+
+// 	                 infoWindow.setPosition(pos);
+// 	                 infoWindow.setContent('Location found.');
+// 	                 map.setCenter(pos);
+// 	               }, function() {
+// 	                 handleLocationError(true, infoWindow, map.getCenter());
+// 	               });
+// 	             } else {
+// 	               // Browser doesn't support Geolocation
+// 	               handleLocationError(false, infoWindow, map.getCenter());
+// 	             }
+// 	           }
+
+// 	           function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+// 	             infoWindow.setPosition(pos);
+// 	             infoWindow.setContent(browserHasGeolocation ?
+// 	                                   'Error: The Geolocation service failed.' :
+// 	                                   'Error: Your browser doesn\'t support geolocation.');
+// 	           }
+	
+// }
+
 giftApp.events = () => {
-	console.log('hello');
-	giftApp.getUserLocation();
+	giftApp.getLcboProductReturn();
 }
 
 giftApp.init = () => {
