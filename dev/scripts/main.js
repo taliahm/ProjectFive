27874c71394d36c62b26b771ebc608e23abc9971
Jessica Del Grande <jessica.delgrande@gmail.com>
2017-03-08 17:47:32 -0500

    THE MAP KNOWS MY LOCATION HOLY SHIT

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -162,18 +162,51 @@ giftApp.getLcboProductReturnTwo = function(firstArrayReturn, userInput) {
 	
 // }
 
-//Jessica is testing map things
+// this map script creates and displays the map object, don't mess with it!
 
 giftApp.map;
 
 giftApp.initMap = () => {
-  // Create a map object and specify the DOM element for display.
-   giftApp.map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -34.397, lng: 150.644},
-    // scrollwheel: false,
-    zoom: 8
-  });
-}
+	giftApp.map = new google.maps.Map(document.getElementById('map'), {
+		center: {lat: -34.397, lng: 150.644},
+		// scrollwheel: false,
+		zoom: 6
+		});
+
+// geolocation script below
+
+const infoWindow = new google.maps.InfoWindow({
+	map: giftApp.map
+});
+
+    // Try HTML5 geolocation.
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        const pos = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude
+        };
+
+        infoWindow.setPosition(pos);
+        infoWindow.setContent('Location found.');
+        giftApp.map.setCenter(pos);
+      }, function() {
+        handleLocationError(true, infoWindow, giftApp.map.getCenter());
+      });
+    } else {
+      // Browser doesn't support Geolocation
+      handleLocationError(false, infoWindow, giftApp.map.getCenter());
+    }
+  }
+
+  function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(browserHasGeolocation ?
+                          'Error: The Geolocation service failed.' :
+                          'Error: Your browser doesn\'t support geolocation.');
+  }
+
+// end geolocation script
 
 //EVENTS
 giftApp.getUserChoice = () => {
