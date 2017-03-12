ae9f681829e808b070955359e54d71caef7eb286
Jessica Del Grande <jessica.delgrande@gmail.com>
2017-03-12 14:41:25 -0400

    fixed lots of to-do list items

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -809,34 +809,6 @@ const alcoholResults = [
       "has_clearance_sale": false,
       "product_no": 211227
     }]
-// THE PSEUDO CODE YO
-
-// User selects alcohol type
-// User selects budget parameter
-// User selects occasion parameter
-// User confirms above selections
-
-	// Make Ajax call to LCBO API with users alcohol type
-	// Take the results array, filter based on budget parameter, return new array
-	// With new array, sort by alcohol content using underscore JS
-	 //if occasion stress=true, take top half of array(meaning the highest alcohol content)
-	 //Top half of array is shuffled
-	 //6 results are displayed (hidden radio buttons, results are wrapped in label)
-	 //if occasion stress=false, take bottom half of array (meaning the lowest alcohol content)
-	 //Bottom half of array is shuffled
-	 //6 results are displayed (hidden radio buttons, results are wrapped in label)
-
-// Users selects 1 of 6 possible products
-// User confirms selection using button
-	//on button click, user is prompted to share their location and product ID is stored
-	//if user rejects, then user is prompted to enter location manually
-	//THIS IS UNCONFIRMED: Ajax call is made to Geolocation to return the lat long of the user location(maybe lat long is already returned when user inputs location)
-	//Using LCBO API "Stores with product" of user choice, return LCBO stores with the product
-	//Compare Lat long of returned LCBO stores to User's lat long (using jsunderscores? _.map _.reduce)
-// Display 3 closest locations with inventory that matches users selection
-
-
-//We will create object of occasions and add stress level as a property with either true or false
 
 //Name spacing
 const giftApp = {};
@@ -1015,10 +987,7 @@ giftApp.getLcboStoresTwo = function(id, firstResult) {
 	})
 };
 
-
-
-
-
+// if user manually enters location
 giftApp.getUsersLocationManual = function() {
         var input = $('#autocomplete')[0];
         var autocomplete = new google.maps.places.Autocomplete(input);
@@ -1067,9 +1036,9 @@ giftApp.map;
 
 giftApp.initMap = () => {
 	giftApp.map = new google.maps.Map(document.getElementById('map'), {
-		center: {lat: 43.6532, lng: -79.3832},
+		center: {lat: 43.648342, lng: -79.398966},
 		scrollwheel: false,
-		zoom: 8, 
+		zoom: 13, 
             styles: [{"featureType":"administrative","elementType":"labels.text.fill","stylers":[{"color":"#444444"}]},{"featureType":"administrative.country","elementType":"all","stylers":[{"saturation":"0"}]},{"featureType":"landscape","elementType":"all","stylers":[{"color":"#d6d4d4"}]},{"featureType":"poi","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"all","stylers":[{"saturation":-100},{"lightness":45}]},{"featureType":"road.highway","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"road.arterial","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#0f4e84"},{"visibility":"on"}]},{"featureType":"water","elementType":"labels.text.fill","stylers":[{"lightness":"11"},{"saturation":"18"}]}]
 		});
 	giftApp.distanceMatrix = new google.maps.DistanceMatrixService(); //distance matrix being woken up
